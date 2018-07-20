3838b44674d8c451db288ba4236b40feb6ef7cd8
qfstudy <qifeistudy@gmail.com>
2018-07-20 16:37:53 +0800

    quna

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
+//路由就是网址不同返回的内容不同
