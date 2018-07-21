746166970eb0ab28a5e0e53ce33c4d3e1303255f
qfstudy <qifeistudy@gmail.com>
2018-07-21 17:00:51 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   render: h => h(App)
 })
-//路由就是网址不同返回的内容不同
+
